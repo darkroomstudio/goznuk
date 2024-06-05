e7eb19faaa77e25263c95e9fb1ffda299f994eef
Jiwon Choi <devjiwonchoi@gmail.com>
2024-06-06 04:16:52 +0900

    chore: setup vscode settings and geist font (#3)
    * geist
    
    * chore: Add VS Code extensions and settings
    
    This commit adds the `.vscode/extensions.json` file to recommend useful VS Code extensions for linting, formatting, showing TODO comments, and checking spelling and grammar. It also adds the `.vscode/settings.json` file to configure TypeScript, formatting, linting, terminal history, todo-tree, file nesting, and cSpell. Additionally, the `package.json` file is updated to specify the required Node.js and pnpm versions.
    
    Note: This commit message is based on the provided code changes and recent repository commits.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export default function RootLayout({
@@ -7,16 +9,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>{children}</body>
     </html>
   )
 }
 
 export const metadata: Metadata = {
-  title: {
-    template: '%s | DKRM Studio',
-    default: 'DKRM Studio',
-  },
-  description: 'Light shines brighter in the dark.',
+  title: 'Goznuk',
 }
