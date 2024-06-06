40112744d35accb7ad6790ba46872a8a7a23d68e
Jeongho Yang <wjdwjdgh6998@gmail.com>
2024-06-07 02:48:53 +0900

    feat: subscribers (#9)
    * feat: subscribers
    
    * refactor: change interfaces to types for consistency

---
+++ src/app/api/v0/users/[username]/subscribers/route.ts
@@ -0,0 +1,50 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+type Subscriber = {
+  id: string
+  name: string
+  username: string
+}
+
+type SubsData = {
+  [key: string]: Subscriber[]
+}
+
+async function getSubscriber(username: string) {
+  const subsData: SubsData = {
+    ggkim0614: [
+      {
+        id: '0',
+        name: 'Jiwon Choi',
+        username: 'devjiwonchoi',
+      },
+      {
+        id: '1',
+        name: 'Jeongho Yang',
+        username: 'jeonghoyang',
+      },
+    ],
+    devjiwonchoi: [
+      {
+        id: '1',
+        name: 'Jeongho Yang',
+        username: 'jeonghoyang',
+      },
+      {
+        id: '2',
+        name: 'George Kim',
+        username: 'ggkim0614',
+      },
+    ],
+  }
+
+  return subsData[username] || []
+}
+
+type Context = { params: { username: string } }
+
+export async function GET(req: NextRequest, { params: { username } }: Context) {
+  const subscribers = await getSubscriber(username)
+  return NextResponse.json({ subscribers })
+}
