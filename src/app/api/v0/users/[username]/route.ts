d97d6d4091f4966a97bd27cd00a9e2e8559e314d
jeonghoyang12 <wjdwjdgh6998@gmail.com>
2024-06-12 14:23:14 +0900

    refactor: add type for the import

--- src/app/api/v0/users/[username]/route.ts
+++ src/app/api/v0/users/[username]/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from 'next/server'
+import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
 import { createClient } from '@/db/supabase/server'
