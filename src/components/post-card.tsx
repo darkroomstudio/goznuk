cc466093023cfd23fa32555c78b57c22b18d7b55
Jiwon Choi <devjiwonchoi@gmail.com>
2024-07-18 13:56:07 +0900

    refactor: pass data as prop (#22)

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,33 +1,44 @@
 import { Badge } from '@/components/ui/badge'
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
 
-export function PostCard() {
+export function PostCard({
+  username,
+  date,
+  readTime,
+  title,
+  categories,
+  avatarImageURL,
+  description,
+}: {
+  username: string
+  date: string
+  readTime: string
+  title: string
+  categories: string[]
+  avatarImageURL: string
+  description: string
+}) {
   return (
     <>
       <div className="flex space-x-2">
-        <Badge variant="secondary">MDX</Badge>
-        <Badge variant="secondary">Markdown</Badge>
-        <Badge variant="secondary">Blogging</Badge>
+        {categories?.map((category) => (
+          <Badge key={category} variant="secondary">
+            {category}
+          </Badge>
+        ))}
       </div>
       <div>
-        <h2 className="text-xl font-bold">
-          How to write an article effectively with utilizing MDX
-        </h2>
-        <p className="text-muted-foreground">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived...
-        </p>
+        <h2 className="text-xl font-bold">{title}</h2>
+        <p className="text-muted-foreground">{description}</p>
       </div>
       <div className="text-muted-foreground flex items-center space-x-2 text-sm">
         <Avatar>
-          <AvatarImage src="/placeholder-user.jpg" />
+          <AvatarImage src={avatarImageURL} />
           <AvatarFallback>LR</AvatarFallback>
         </Avatar>
-        <span>Lee Robinson</span>
-        <span>05/22/2024</span>
-        <span>5min read</span>
+        <span>{username}</span>
+        <span>{date}</span>
+        <span>{readTime}min read</span>
       </div>
     </>
   )
